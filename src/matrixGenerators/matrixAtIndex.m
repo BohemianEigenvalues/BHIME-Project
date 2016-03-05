15be0953e8f3c8d05b9b38f282a9b4717fa31aea
Steven Thornton <steven@steventhornton.ca>
2016-03-05 11:27:39 -0500

    Added license

--- src/matrixGenerators/matrixAtIndex.m
+++ src/matrixGenerators/matrixAtIndex.m
@@ -1,27 +1,41 @@
-%-------------------------------------------------------------------------
-% AUTHOR .... Steven E. Thornton
-% EMAIL ..... sthornt7@uwo.ca
-% UPDATED ... Mar. 5/2016
-%
-% This function generates the square matrix with entries from a population
-% vector such that:
-%   - The population vector is of length b
-%   - The max number of unique matrices is N = b^(n^2)
-%   - Let R = i mod N in base b
-%   - Reverse R and pad on the left with zeros to give a number with n^2
-%     digits
-%   - This number then gives the indices of the population vector for each 
-%     element of the output matrix
-%
-% INPUT
-%	i ............ Index to select matrix at
-%	n ............ Size of matrix
-%	population ... Vector of values to be used in matrix
-%
-% OUTPUT
-%   A square matrix where the entries are sampled from the population and
-%   determined uniquely for each value of i mod N.
-%-------------------------------------------------------------------------
+% ------------------------------------------------------------------------- %
+% AUTHOR .... Steven E. Thornton (Copyright (c) 2016)                       %
+% EMAIL ..... sthornt7@uwo.ca                                               %
+% UPDATED ... Mar. 5/2016                                                   %
+%                                                                           %
+% This function generates the square matrix with entries from a population  %
+% vector such that:                                                         %
+%   - The population vector is of length b                                  %
+%   - The max number of unique matrices is N = b^(n^2)                      %
+%   - Let R = i mod N in base b                                             %
+%   - Reverse R and pad on the left with zeros to give a number with n^2    %
+%     digits                                                                %
+%   - This number then gives the indices of the population vector for each  %
+%     element of the output matrix                                          %
+%                                                                           %
+% INPUT                                                                     %
+%	i ............ Index to select matrix at                                %
+%	n ............ Size of matrix                                           %
+%	population ... Vector of values to be used in matrix                    %
+%                                                                           %
+% OUTPUT                                                                    %
+%   A square matrix where the entries are sampled from the population and   %
+%   determined uniquely for each value of i mod N.                          %
+%                                                                           %
+% LICENSE                                                                   %
+%	This program is free software: you can redistribute it and/or modify    %
+%	it under the terms of the GNU General Public License as published by    %
+% 	the Free Software Foundation, either version 3 of the License, or       %
+% 	any later version.                                                      %
+%                                                                           %
+% 	This program is distributed in the hope that it will be useful,         %
+% 	but WITHOUT ANY WARRANTY; without even the implied warranty of          %
+% 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           %
+% 	GNU General Public License for more details.                            %
+%                                                                           %
+% 	You should have received a copy of the GNU General Public License       %
+% 	along with this program.  If not, see http://www.gnu.org/licenses/.     %
+% ------------------------------------------------------------------------- %
 function A = matrixAtIndex(i, n, population)
     
     % Number of values in population vector
